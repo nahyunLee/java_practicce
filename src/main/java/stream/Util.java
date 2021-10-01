be855215cdc09741a8358e723d0a64d85f73a452
nahyunLee <sksms304@gmail.com>
2021-10-01 19:40:05 +0900

    예제에 공통적으로 등장하는 코드들을 담은 Util생성

---
+++ src/main/java/stream/Util.java
@@ -0,0 +1,18 @@
+package stream;
+
+import java.util.stream.Stream;
+
+public class Util {
+    public Student[] makeStudentArray(){
+        Student[] studentStream = {
+                new Student("이자바", 3,300),
+                new Student("김자바", 1,200),
+                new Student("안자바", 2,100),
+                new Student("박자바", 2,150),
+                new Student("소자바", 1,200),
+                new Student("나자바", 3,290),
+                new Student("감자바", 3,180)
+    };
+        return studentStream;
+    }
+}
