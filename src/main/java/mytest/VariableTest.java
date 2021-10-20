982fa63c4f013d390f0e1333a18ee53b9873c44f
이나현 <nahyun@wemakeprice.com>
2021-10-20 15:29:11 +0900

    멤버변수 초기화 확인

--- src/main/java/mytest/VariableTest.java
+++ src/main/java/mytest/VariableTest.java
@@ -5,8 +5,14 @@ public class VariableTest {
     private static String noInit;
     private static String init = "hello";
     public static void main(String[] args){
+        String hello ="";
+        String nullHello;
         System.out.println(initTest);
         System.out.println(noInit);
         System.out.println(init);
+        System.out.println("".equals(noInit));
+
+        hello = "hi";
+        System.out.println(hello);
     }
 }
